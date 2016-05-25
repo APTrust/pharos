cb8220b7461afca3576c458d0a8fbce037a9db7f
Kelly Croswell <kelly.croswell@aptrust.org>
2016-05-25 15:49:17 -0400

    testing use case for hydra role management without hydra which as it turns out will not work

--- Gemfile
+++ Gemfile
@@ -32,7 +32,6 @@ gem 'valid_email'
 
 #gem 'omniauth-google-oauth2'
 gem 'simple_form', '~> 3.0.2'
-#gem "hydra-role-management", "~> 0.1.0"
 gem 'phony_rails'
 gem 'inherited_resources'
 gem 'uuidtools'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  mount Hydra::RoleManagement::Engine => '/'
+
   resources :institutions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
