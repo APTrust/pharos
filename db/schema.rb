0d5b0a3bdb6d2f0e31209ac67eb8b51104bfd6cb
Kelly Croswell <kelly.croswell@aptrust.org>
2016-05-26 16:00:11 -0400

    added api key column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526192033) do
+ActiveRecord::Schema.define(version: 20160526195857) do
 
   create_table "checksums", force: :cascade do |t|
     t.string   "algorithm"
@@ -107,18 +107,19 @@ ActiveRecord::Schema.define(version: 20160526192033) do
     t.string   "email"
     t.string   "phone_number"
     t.string   "institution"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.string   "encrypted_password",       default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",            default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.integer  "institution_id"
+    t.text     "encrypted_api_secret_key"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
