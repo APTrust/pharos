d1d36ae61b8af675a19baa3e38de53fbf2b997e3
Kelly Croswell <kelly.croswell@aptrust.org>
2016-06-02 16:11:14 -0400

    added statistics sampler module

---
+++ lib/statistics_sampler.rb
@@ -0,0 +1,10 @@
+module StatisticsSampler
+
+  # Record a list of all the current statistics in the database
+  def self.record_current_statistics
+    Institution.all.each do |inst|
+      UsageSample.create(institution_id: inst.pid)
+    end
+  end
+
+end
