343806472271d6e5f9173236ab6c6de2a513acc5
Kelly Croswell <kelly.croswell@aptrust.org>
2016-06-24 16:33:34 -0400

    added work item helper code

--- app/helpers/work_items_helper.rb
+++ app/helpers/work_items_helper.rb
@@ -1,2 +1,21 @@
 module WorkItemsHelper
+  def current_path(param, value)
+    old_path = request.fullpath
+    if old_path.include? 'wi_sort'
+      new_path = url_for(params.except :wi_sort)
+    else
+      new_path = old_path
+    end
+    if new_path.include? '?'
+      path = "#{new_path}&#{param}=#{value}"
+    else
+      path = "#{new_path}?#{param}=#{value}"
+    end
+    if new_path.include? 'search'
+      unless new_path.include? 'qq'
+        path = "#{path}&search_field=#{params[:search_field]}&qq=#{params[:qq]}"
+      end
+    end
+    path
+  end
 end
