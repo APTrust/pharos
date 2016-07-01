8ef6748c6bd91ededf0d9cfc628bd76c5f3d5668
Kelly Croswell <kelly.croswell@aptrust.org>
2016-07-01 13:48:33 -0400

    work on shared views and intellectual objects, decrypting blacklight nonsense

--- app/helpers/intellectual_objects_helper.rb
+++ app/helpers/intellectual_objects_helper.rb
@@ -6,4 +6,12 @@ module IntellectualObjectsHelper
   def format_class(format)
     format.split('/')[-1].downcase.gsub(/\s/, '_') + '_label' unless format.split('/')[-1].nil?
   end
+
+  def query_has_constraints?(localized_params = params)
+    !(localized_params[:q].blank? and localized_params[:f].blank?)
+  end
+
+  def search_action_url options = {}
+    institution_intellectual_objects_path(institution_identifier: params[:identifier])
+  end
 end
