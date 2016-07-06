e7b92a5b40658a959c61e3efb9e66bf669dd1f47
Kelly Croswell <kelly.croswell@aptrust.org>
2016-07-06 16:35:57 -0400

    off track attempt at fixing a flash notice, will fix later

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,17 +8,16 @@ describe 'Users' do
 
   describe 'DELETE users', :type => :feature do
     before do
+      User.delete_all
       @user = FactoryGirl.create(:user, :admin)
       @user2 = FactoryGirl.create(:user)
     end
 
-    it 'should provide message after delete with name of deleted instituion' do
+    it 'should provide message after delete with name of deleted user' do
       login_as(@user)
       visit('/users')
       expect {
-        within(:xpath, "//tr[@id='#{@user2.id}']") do
-          click_link 'Delete'
-        end
+        click_link 'Delete'
       }.to change(User, :count).by(-1)
       page.should have_content "#{@user2.name} was deleted."
     end
