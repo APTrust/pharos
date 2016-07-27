e687fb6c05c8aa1258a9e9a34222d66414632674
A. Diamond <diamond@sheprador.com>
2016-07-27 15:13:41 -0400

    Fixed 2 routing tests for intel obj

--- spec/routing/intellectual_objects_routing_spec.rb
+++ spec/routing/intellectual_objects_routing_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'Routing' do
   it 'should route to the index when GET /objects' do
-    expect(get: '/objects/aptrust.org').to route_to(controller: 'intellectual_objects', action: 'index', institution_identifier: 'aptrust.org')
+    expect(get: '/objects').to route_to(controller: 'intellectual_objects', action: 'index')
   end
   it 'should route to the show page when GET /objects/aptrust.org/12345678' do
     expect(get: '/objects/aptrust.org%2F12345678').to route_to(controller: 'intellectual_objects', action: 'show', intellectual_object_identifier: 'aptrust.org/12345678')
     expect(intellectual_object_path('aptrust.org/12345678')).to eq '/objects/aptrust.org%2F12345678'
   end
   it 'should route to create when POST /objects/aptrust.org' do
-    expect(post: '/objects/aptrust.org').to route_to(controller: 'intellectual_objects', action: 'create', institution_identifier: 'aptrust.org')
+    expect(post: 'api/v2/objects/aptrust.org').to route_to(controller: 'intellectual_objects', action: 'create', institution_identifier: 'aptrust.org')
   end
 end
