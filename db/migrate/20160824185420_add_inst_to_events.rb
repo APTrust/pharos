6896e7a08e5c74790ec33086119e9ccabcac7d05
Kelly Croswell <kelly.croswell@aptrust.org>
2016-08-24 14:58:59 -0400

    commented out part of event migration because it was causing migration to fail

--- db/migrate/20160824185420_add_inst_to_events.rb
+++ db/migrate/20160824185420_add_inst_to_events.rb
@@ -2,6 +2,6 @@ class AddInstToEvents < ActiveRecord::Migration
   def change
     remove_column :premis_events, :outcome
     add_column :premis_events, :outcome, :string
-    add_column :premis_events, :institution_id, :integer
+    #add_column :premis_events, :institution_id, :integer
   end
 end
