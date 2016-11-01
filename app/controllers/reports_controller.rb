b8f841272f6bcfa67c61383f3bdcba663d942330
Kelly Croswell <kelly.croswell@aptrust.org>
2016-11-01 15:53:41 -0400

    figured out missing template error, now need better formatting

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,23 +19,23 @@ class ReportsController < ApplicationController
     respond_to do |format|
       format.json { render json: { report: @report } }
       format.html { }
-
-    end
-  end
-
-  def download
-    authorize @institution, :overview?
-    respond_to do |format|
       format.pdf do
-        html = render_to_string(action: :overview, layout: '/reports/test.html')
+        html = render_to_string(action: :overview, layout: 'reports/overview.pdf.erb')
         pdf = WickedPdf.new.pdf_from_string(html)
-        send_data(pdf, filename: 'filename.pdf', disposition: 'attachment')
+        send_data(pdf, filename: "Overview for #{@institution.name}", disposition: 'attachment')
 
         # save_path = Rails.root.join('pdfs','filename.pdf')
         # File.open(save_path, 'wb') do |file|
         #   file << pdf
         # end
       end
+    end
+  end
+
+  def download
+    authorize @institution, :overview?
+    respond_to do |format|
+
     end
   end
 
