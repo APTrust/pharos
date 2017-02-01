7237d225b49c7766debf6f74594be76d98c34695
Kelly Croswell <kelly.croswell@aptrust.org>
2017-02-01 15:25:01 -0500

    added last fixity to generic_file#show

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170131152046) do
     t.datetime "created_at",                                                       null: false
     t.datetime "updated_at",                                                       null: false
     t.string   "state"
+    t.text     "ingest_state"
     t.datetime "last_fixity_check",                default: '2000-01-01 00:00:00', null: false
   end
 
@@ -82,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170131152046) do
     t.string   "state"
     t.string   "etag"
     t.string   "dpn_uuid"
+    t.text     "ingest_state"
   end
 
   add_index "intellectual_objects", ["access"], name: "index_intellectual_objects_on_access"
@@ -102,8 +104,8 @@ ActiveRecord::Schema.define(version: 20170131152046) do
     t.integer  "generic_file_id"
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
-    t.string   "outcome"
     t.integer  "institution_id"
+    t.string   "outcome"
     t.string   "intellectual_object_identifier", default: "", null: false
     t.string   "generic_file_identifier",        default: "", null: false
     t.string   "old_uuid"
