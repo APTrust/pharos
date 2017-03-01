83d067cfeb39c95e53658180bb26f19f4389087b
Kelly Croswell <kelly.croswell@aptrust.org>
2017-03-01 14:16:22 -0500

    simplified in_dpn?

--- app/models/intellectual_object.rb
+++ app/models/intellectual_object.rb
@@ -102,21 +102,7 @@ class IntellectualObject < ActiveRecord::Base
   end
 
   def in_dpn?
-    if self.dpn_uuid.nil? || self.dpn_uuid.blank? || self.dpn_uuid.empty?
-      object_in_dpn = false
-      dpn = Pharos::Application::PHAROS_ACTIONS['dpn']
-      record = Pharos::Application::PHAROS_STAGES['record']
-      success = Pharos::Application::PHAROS_STATUSES['success']
-      dpn_items = WorkItem.where(object_identifier: self.identifier, action: dpn)
-      dpn_items.each do |item|
-        if item.stage == record && item.status == success
-          object_in_dpn = true
-          break
-        end
-      end
-    else
-      object_in_dpn = true
-    end
+    (self.dpn_uuid.nil? || self.dpn_uuid.blank? || self.dpn_uuid.empty?) ? object_in_dpn = false : object_in_dpn = true
     object_in_dpn
   end
 
