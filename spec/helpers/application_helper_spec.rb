2a82f006902231bab73240d1ad8801f731e50d8b
Kelly Croswell <kelly.croswell@aptrust.org>
2017-10-26 12:10:08 -0400

    added dpn_process_time spec test, added processing time to show view

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -22,5 +22,21 @@ describe ApplicationHelper do
     end
   end
 
+  describe '#dpn_process_time' do
+    it 'should return a proper process time' do
+      queue_date = Time.parse('2017-10-25 18:30:01 UTC')
+      complete_date = Time.parse('2017-10-26 06:03:21 UTC')
+      dpn_item = FactoryGirl.create(:dpn_work_item, queued_at: queue_date, completed_at: complete_date)
+      process_time = dpn_process_time(dpn_item)
+      process_time.should == '11.56 hours'
+
+      queue_date_two = Time.parse('2016-09-28T19:39:39Z')
+      complete_date_two = Time.parse('2016-09-29T05:42:37Z')
+      dpn_item_two = FactoryGirl.create(:dpn_work_item, queued_at: queue_date_two, completed_at: complete_date_two)
+      process_time_two = dpn_process_time(dpn_item_two)
+      process_time_two.should == '10.05 hours'
+    end
+  end
+
 end
 
