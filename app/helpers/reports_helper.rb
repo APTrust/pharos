3b58fdbd5227a349f70ccad700c77b6b618fcd99
Kelly Croswell <kelly.croswell@aptrust.org>
2018-04-05 11:02:54 -0400

    fixed the way costs are formatted in reports

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -3,7 +3,7 @@ module ReportsHelper
   def cost_analysis_member(size)
     price = ''
     if size < 10995116277760 #10 TB
-      price = '$0.00'
+      price = 0.00
     else
       excess = size - 10995116277760
       price = cost_analysis_subscriber(excess)
@@ -13,9 +13,8 @@ module ReportsHelper
 
   def cost_analysis_subscriber(size)
     cost = size * 0.000000000381988
-    rounded_cost = cost.round(2)
-    price = "$#{rounded_cost}"
-    price = '$0.00' if price == '$0.0'
+    price = cost.round(2)
     price
   end
+
 end
