4f4a836bbbb04c6e087123d0250cfbcef9a74a65
Kelly Croswell <kelly.croswell@aptrust.org>
2018-04-17 16:43:48 -0400

    initial analysis on mimetype data

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -17,4 +17,50 @@ module ReportsHelper
     price
   end
 
+  def mimetype_analysis
+    @application_report = {}
+    @audio_report = {}
+    @image_report = {}
+    @video_report = {}
+    @text_report = {}
+    @other_report = {}
+    application_total = 0
+    audio_total = 0
+    image_total = 0
+    video_total = 0
+    text_total = 0
+    other_total = 0
+    @base_report = {}
+    @mimetype_report = Hash[@mimetype_report.sort]
+    @mimetype_report.each do |mimetype, count|
+      base_type = mimetype.split('/')[0]
+      case base_type
+        when 'application'
+          @application_report[mimetype] = count
+          application_total += count
+        when 'audio'
+          @audio_report[mimetype] = count
+          audio_total += count
+        when 'video'
+          @video_report[mimetype] = count
+          video_total += count
+        when 'image'
+          @image_report[mimetype] = count
+          image_total += count
+        when 'text'
+          @text_report[mimetype] = count
+          text_total += count
+        else
+          @other_report[mimetype] = count
+          other_total += count
+      end
+    end
+    @base_report['Applications'] = application_total
+    @base_report['Audio'] = audio_total
+    @base_report['Video'] = video_total
+    @base_report['Image'] = image_total
+    @base_report['Text'] = text_total
+    @base_report['Other'] = other_total
+  end
+
 end
