efbc0ccf8ed24f7fa3a719c81214983b0dee991b
A. Diamond <diamond@sheprador.com>
2018-05-08 14:40:28 -0400

    Simplified storage_option migrations

--- db/migrate/20180412183208_add_storage_type_to_generic_files.rb
+++ db/migrate/20180412183208_add_storage_type_to_generic_files.rb
@@ -1,20 +1,7 @@
 class AddStorageTypeToGenericFiles < ActiveRecord::Migration[5.1]
   def up
     puts "This migration takes a long time to run!"
-    add_column :generic_files, :storage_type, :string, default: 'Standard'
-
-    batch_size = 5000
-    count = 0
-    GenericFile.find_in_batches(batch_size: batch_size) do |batch|
-      GenericFile.transaction do
-        batch.each do |gf|
-          gf.storage_type = 'standard'
-          gf.save!
-        end
-      end
-      count += 1
-      puts "[#{Time.now}] Updated #{count * batch_size} GenericFiles"
-    end
+    add_column :generic_files, :storage_type, :string, null: false, default: 'Standard'
   end
 
   def down
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "checksums", force: :cascade do |t|
+  create_table "checksums", id: :serial, force: :cascade do |t|
     t.string "algorithm"
     t.string "datetime"
     t.string "digest"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "dpn_work_items", force: :cascade do |t|
+  create_table "dpn_work_items", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remote_node", limit: 20, default: "", null: false
@@ -74,20 +74,20 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
   end
 
   create_table "emails_premis_events", id: false, force: :cascade do |t|
-    t.integer "premis_event_id"
-    t.integer "email_id"
+    t.bigint "premis_event_id"
+    t.bigint "email_id"
     t.index ["email_id"], name: "index_emails_premis_events_on_email_id"
     t.index ["premis_event_id"], name: "index_emails_premis_events_on_premis_event_id"
   end
 
   create_table "emails_work_items", id: false, force: :cascade do |t|
-    t.integer "work_item_id"
-    t.integer "email_id"
+    t.bigint "work_item_id"
+    t.bigint "email_id"
     t.index ["email_id"], name: "index_emails_work_items_on_email_id"
     t.index ["work_item_id"], name: "index_emails_work_items_on_work_item_id"
   end
 
-  create_table "generic_files", force: :cascade do |t|
+  create_table "generic_files", id: :serial, force: :cascade do |t|
     t.string "file_format"
     t.string "uri"
     t.bigint "size"
@@ -96,10 +96,10 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "state"
-    t.text "ingest_state"
     t.datetime "last_fixity_check", default: "2000-01-01 00:00:00", null: false
+    t.text "ingest_state"
     t.integer "institution_id", null: false
-    t.string "storage_option", default: "standard"
+    t.string "storage_option", default: "Standard", null: false
     t.index ["created_at"], name: "index_generic_files_on_created_at"
     t.index ["file_format", "state"], name: "index_generic_files_on_file_format_and_state"
     t.index ["file_format"], name: "index_generic_files_on_file_format"
@@ -119,7 +119,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.index ["updated_at"], name: "index_generic_files_on_updated_at"
   end
 
-  create_table "institutions", force: :cascade do |t|
+  create_table "institutions", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "brief_name"
     t.string "identifier"
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.index ["name"], name: "index_institutions_on_name"
   end
 
-  create_table "intellectual_objects", force: :cascade do |t|
+  create_table "intellectual_objects", id: :serial, force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "identifier"
@@ -147,7 +147,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.string "dpn_uuid"
     t.text "ingest_state"
     t.string "bagging_group_identifier", limit: 255
-    t.string "storage_option", default: "standard"
+    t.string "storage_option", default: "Standard", null: false
     t.index ["access"], name: "index_intellectual_objects_on_access"
     t.index ["bag_name"], name: "index_intellectual_objects_on_bag_name"
     t.index ["created_at"], name: "index_intellectual_objects_on_created_at"
@@ -158,7 +158,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.index ["updated_at"], name: "index_intellectual_objects_on_updated_at"
   end
 
-  create_table "premis_events", force: :cascade do |t|
+  create_table "premis_events", id: :serial, force: :cascade do |t|
     t.string "identifier"
     t.string "event_type"
     t.string "date_time"
@@ -190,7 +190,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.index ["outcome"], name: "index_premis_events_on_outcome"
   end
 
-  create_table "roles", force: :cascade do |t|
+  create_table "roles", id: :serial, force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -213,14 +213,14 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.string "snapshot_type"
   end
 
-  create_table "usage_samples", force: :cascade do |t|
+  create_table "usage_samples", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "institution_id"
     t.text "data"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone_number"
@@ -242,7 +242,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "work_item_states", force: :cascade do |t|
+  create_table "work_item_states", id: :serial, force: :cascade do |t|
     t.integer "work_item_id"
     t.string "action", null: false
     t.binary "state"
@@ -250,7 +250,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_140355) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "work_items", force: :cascade do |t|
+  create_table "work_items", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "intellectual_object_id"
