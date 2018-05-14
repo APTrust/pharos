2912b02c649e38898216aa5662f447d0e1a337f4
A. Diamond <diamond@sheprador.com>
2018-05-14 15:16:42 -0400

    Changed self.saved_change_to_storage_option to self.storage_option_changed so spec tests pass

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -31,170 +31,170 @@ class GenericFile < ActiveRecord::Base
   scope :with_state, ->(param) { where(state: param) unless (param.blank? || param == 'all' || param == 'All') }
   scope :with_storage_option, ->(param) { where(storage_option: param) unless param.blank? }
   scope :with_access, ->(param) {
-    joins(:intellectual_object)
-        .where('intellectual_objects.access = ?', param) unless param.blank?
+	joins(:intellectual_object)
+		.where('intellectual_objects.access = ?', param) unless param.blank?
   }
   scope :discoverable, ->(current_user) {
-    joins(:intellectual_object)
-    .where('intellectual_objects.institution_id = ?', current_user.institution.id) unless current_user.admin?
+	joins(:intellectual_object)
+	.where('intellectual_objects.institution_id = ?', current_user.institution.id) unless current_user.admin?
   }
   scope :readable, ->(current_user) {
-    # Inst admin can read anything at their institution.
-    # Inst user can read read any unrestricted item at their institution.
-    # Admin can read anything.
-    if current_user.institutional_admin?
-      joins(:intellectual_object)
-      .where('intellectual_objects.institution_id = ?', current_user.institution.id)
-    elsif current_user.institutional_user?
-      joins(:intellectual_object)
-      .where("(intellectual_objects.access != 'restricted' and intellectual_objects.institution_id = ?)", current_user.institution.id)
-    end
+	# Inst admin can read anything at their institution.
+	# Inst user can read read any unrestricted item at their institution.
+	# Admin can read anything.
+	if current_user.institutional_admin?
+	  joins(:intellectual_object)
+	  .where('intellectual_objects.institution_id = ?', current_user.institution.id)
+	elsif current_user.institutional_user?
+	  joins(:intellectual_object)
+	  .where("(intellectual_objects.access != 'restricted' and intellectual_objects.institution_id = ?)", current_user.institution.id)
+	end
   }
   scope :writable, ->(current_user) {
-    # Only admin has write privileges for now.
-    where('(1 = 0)') unless current_user.admin?
+	# Only admin has write privileges for now.
+	where('(1 = 0)') unless current_user.admin?
   }
 
   def self.find_by_identifier(identifier)
-    return nil if identifier.blank?
-    unescaped_identifier = identifier.gsub(/%2F/i, '/')
-    file = GenericFile.where(identifier: unescaped_identifier).first
-    # if file.nil?
-    #   #check to see if there's a %3A that got turned into a colon by overeager rails
-    #   no_colon_identifier = unescaped_identifier.gsub(/:/, '%3A')
-    #   file = GenericFile.where(identifier: no_colon_identifier).first
-    # end
-    file
+	return nil if identifier.blank?
+	unescaped_identifier = identifier.gsub(/%2F/i, '/')
+	file = GenericFile.where(identifier: unescaped_identifier).first
+	# if file.nil?
+	#   #check to see if there's a %3A that got turned into a colon by overeager rails
+	#   no_colon_identifier = unescaped_identifier.gsub(/:/, '%3A')
+	#   file = GenericFile.where(identifier: no_colon_identifier).first
+	# end
+	file
   end
 
   def to_param
-    identifier
+	identifier
   end
 
   def self.empty_param(param)
-    (param.blank? || param.nil? || param == '*' || param == '' || param == '%') ? true : false
+	(param.blank? || param.nil? || param == '*' || param == '' || param == '%') ? true : false
   end
 
   def self.bytes_by_format
-    stats = GenericFile.sum(:size)
-    if stats
-      cross_tab = GenericFile.group(:file_format).sum(:size)
-      cross_tab['all'] = stats
-      cross_tab
-    else
-      {'all' => 0}
-    end
+	stats = GenericFile.sum(:size)
+	if stats
+	  cross_tab = GenericFile.group(:file_format).sum(:size)
+	  cross_tab['all'] = stats
+	  cross_tab
+	else
+	  {'all' => 0}
+	end
 
   end
 
   def display
-    identifier
+	identifier
   end
 
   def institution
-    self.intellectual_object.institution
+	self.intellectual_object.institution
   end
 
   def soft_delete(attributes)
-    user_email = attributes[:outcome_detail]
-    attributes[:identifier] = SecureRandom.uuid
-    io = IntellectualObject.find(self.intellectual_object_id)
-    WorkItem.create_delete_request(io.identifier,
-                                   self.identifier,
-                                   user_email)
-    self.state = 'D'
-    # Let exchange services create the file delete
-    # event when it actually deletes the file.
-    # self.add_event(attributes)
-    self.save!
+	user_email = attributes[:outcome_detail]
+	attributes[:identifier] = SecureRandom.uuid
+	io = IntellectualObject.find(self.intellectual_object_id)
+	WorkItem.create_delete_request(io.identifier,
+								   self.identifier,
+								   user_email)
+	self.state = 'D'
+	# Let exchange services create the file delete
+	# event when it actually deletes the file.
+	# self.add_event(attributes)
+	self.save!
   end
 
   # This is for serializing JSON in the API.
   def serializable_hash(options={})
-    data = super(options)
-    data.delete('ingest_state')
-    if options.has_key?(:include) && options[:include].include?(:ingest_state)
-      if self.ingest_state.nil?
-        data['ingest_state'] = 'null'
-      else
-        state = JSON.parse(self.ingest_state)
-        data.merge!(ingest_state: state)
-      end
-    end
-    data['intellectual_object_identifier'] = self.intellectual_object.identifier
-    if options.has_key?(:include)
-      data.merge!(checksums: serialize_checksums) if options[:include].include?(:checksums)
-      data.merge!(premis_events: serialize_events) if options[:include].include?(:premis_events)
-    end
-    data
+	data = super(options)
+	data.delete('ingest_state')
+	if options.has_key?(:include) && options[:include].include?(:ingest_state)
+	  if self.ingest_state.nil?
+		data['ingest_state'] = 'null'
+	  else
+		state = JSON.parse(self.ingest_state)
+		data.merge!(ingest_state: state)
+	  end
+	end
+	data['intellectual_object_identifier'] = self.intellectual_object.identifier
+	if options.has_key?(:include)
+	  data.merge!(checksums: serialize_checksums) if options[:include].include?(:checksums)
+	  data.merge!(premis_events: serialize_events) if options[:include].include?(:premis_events)
+	end
+	data
   end
 
   def serialize_checksums
-    checksums.map do |cs|
-      cs.serializable_hash
-    end
+	checksums.map do |cs|
+	  cs.serializable_hash
+	end
   end
 
   def add_event(attributes)
-    event = self.premis_events.build(attributes)
-    event.generic_file = self
-    event.intellectual_object = self.intellectual_object
-    event.institution = self.intellectual_object.institution
-    event.save!
-    event
+	event = self.premis_events.build(attributes)
+	event.generic_file = self
+	event.intellectual_object = self.intellectual_object
+	event.institution = self.intellectual_object.institution
+	event.save!
+	event
   end
 
   def serialize_events
-    premis_events.map do |event|
-      event.serializable_hash
-    end
+	premis_events.map do |event|
+	  event.serializable_hash
+	end
   end
 
   # Returns the checksum with the specified digest, or nil.
   # No need to specify algorithm, since we're using md5 and sha256,
   # and their digests have different lengths.
   def find_checksum_by_digest(digest)
-    checksum = nil
-    checksums.each do |cs|
-      if cs.digest == digest
-        checksum = cs
-        break
-      end
-    end
-    checksum
+	checksum = nil
+	checksums.each do |cs|
+	  if cs.digest == digest
+		checksum = cs
+		break
+	  end
+	end
+	checksum
   end
 
   # Returns true if the GenericFile has a checksum with the specified digest.
   def has_checksum?(digest)
-    find_checksum_by_digest(digest).nil? == false
+	find_checksum_by_digest(digest).nil? == false
   end
 
   private
 
   def init_institution_id
-    unless self.intellectual_object.nil?
-      self.institution_id = self.intellectual_object.institution_id if self.institution_id.nil?
-    end
+	unless self.intellectual_object.nil?
+	  self.institution_id = self.intellectual_object.institution_id if self.institution_id.nil?
+	end
   end
 
   def matching_storage_option
-    unless self.intellectual_object.nil?
-      self.storage_option = self.intellectual_object.storage_option
-    end
+	unless self.intellectual_object.nil?
+	  self.storage_option = self.intellectual_object.storage_option
+	end
   end
 
   def freeze_institution_id
-    errors.add(:institution_id, 'cannot be changed') if self.saved_change_to_institution_id? unless self.institution_id.nil?
+	errors.add(:institution_id, 'cannot be changed') if self.institution_id_changed? unless self.institution_id.nil?
   end
 
   def freeze_storage_option
-    errors.add(:storage_option, 'cannot be changed') if self.saved_change_to_storage_option? unless self.storage_option.nil?
+	errors.add(:storage_option, 'cannot be changed') if self.storage_option_changed? unless self.storage_option.nil?
   end
 
   def storage_option_is_allowed
-    unless Pharos::Application::PHAROS_STORAGE_OPTIONS.include?(self.storage_option)
-      errors.add(:storage_option, 'Storage Option is not one of the allowed options')
-    end
+	unless Pharos::Application::PHAROS_STORAGE_OPTIONS.include?(self.storage_option)
+	  errors.add(:storage_option, 'Storage Option is not one of the allowed options')
+	end
   end
 
 end
--- app/models/intellectual_object.rb
+++ app/models/intellectual_object.rb
@@ -41,245 +41,245 @@ class IntellectualObject < ActiveRecord::Base
   scope :with_access, ->(param) { where(access: param) unless param.blank? }
   scope :with_storage_option, ->(param) { where(storage_option: param) unless param.blank? }
   scope :with_file_format, ->(param) {
-    joins(:generic_files)
-        .where('generic_files.file_format = ?', param) unless param.blank?
+	joins(:generic_files)
+		.where('generic_files.file_format = ?', param) unless param.blank?
   }
   scope :discoverable, ->(current_user) {
-    # Any user can discover any item at their institution,
-    # along with 'consortia' items from any institution.
-    where("(intellectual_objects.access = 'consortia' or intellectual_objects.institution_id = ?)", current_user.institution.id) unless current_user.admin?
+	# Any user can discover any item at their institution,
+	# along with 'consortia' items from any institution.
+	where("(intellectual_objects.access = 'consortia' or intellectual_objects.institution_id = ?)", current_user.institution.id) unless current_user.admin?
   }
   scope :readable, ->(current_user) {
-    # Inst admin can read anything at their institution.
-    # Inst user can read read any unrestricted item at their institution.
-    # Admin can read anything.
-    if current_user.institutional_admin?
-      where(institution: current_user.institution)
-    elsif current_user.institutional_user?
-      where("(intellectual_objects.access != 'restricted' and intellectual_objects.institution_id = ?)", current_user.institution.id)
-    end
+	# Inst admin can read anything at their institution.
+	# Inst user can read read any unrestricted item at their institution.
+	# Admin can read anything.
+	if current_user.institutional_admin?
+	  where(institution: current_user.institution)
+	elsif current_user.institutional_user?
+	  where("(intellectual_objects.access != 'restricted' and intellectual_objects.institution_id = ?)", current_user.institution.id)
+	end
   }
   scope :writable, ->(current_user) {
-    # Only admin has write privileges for now.
-    where('(1 = 0)') unless current_user.admin?
+	# Only admin has write privileges for now.
+	where('(1 = 0)') unless current_user.admin?
   }
 
   def self.find_by_identifier(identifier)
-    return nil if identifier.blank?
-    unescaped_identifier = identifier.gsub(/%2F/i, '/')
-    IntellectualObject.where(identifier: unescaped_identifier).first
+	return nil if identifier.blank?
+	unescaped_identifier = identifier.gsub(/%2F/i, '/')
+	IntellectualObject.where(identifier: unescaped_identifier).first
   end
 
   def to_param
-    identifier
+	identifier
   end
 
   def self.empty_param(param)
-    (param.blank? || param.nil? || param == '*' || param == '' || param == '%') ? true : false
+	(param.blank? || param.nil? || param == '*' || param == '' || param == '%') ? true : false
   end
 
   def bytes_by_format
-    stats = self.generic_files.sum(:size)
-    if stats
-      cross_tab = self.generic_files.group(:file_format).sum(:size)
-      cross_tab['all'] = stats
-      cross_tab
-    else
-      {'all' => 0}
-    end
+	stats = self.generic_files.sum(:size)
+	if stats
+	  cross_tab = self.generic_files.group(:file_format).sum(:size)
+	  cross_tab['all'] = stats
+	  cross_tab
+	else
+	  {'all' => 0}
+	end
   end
 
   def soft_delete(attributes)
-    attributes[:identifier] = SecureRandom.uuid
-    self.state = 'D'
-    self.add_event(attributes)
-    self.save!
-    unless Rails.env.test?
-      Thread.new() do
-        background_deletion(attributes)
-        ActiveRecord::Base.connection.close
-      end
-    end
+	attributes[:identifier] = SecureRandom.uuid
+	self.state = 'D'
+	self.add_event(attributes)
+	self.save!
+	unless Rails.env.test?
+	  Thread.new() do
+		background_deletion(attributes)
+		ActiveRecord::Base.connection.close
+	  end
+	end
   end
 
   def background_deletion(attributes)
-    generic_files.each do |gf|
-      gf.soft_delete(attributes)
-    end
-    save!
+	generic_files.each do |gf|
+	  gf.soft_delete(attributes)
+	end
+	save!
   end
 
   def in_dpn?
-    (self.dpn_uuid.nil? || self.dpn_uuid.blank? || self.dpn_uuid.empty?) ? object_in_dpn = false : object_in_dpn = true
-    object_in_dpn
+	(self.dpn_uuid.nil? || self.dpn_uuid.blank? || self.dpn_uuid.empty?) ? object_in_dpn = false : object_in_dpn = true
+	object_in_dpn
   end
 
   def dpn_bag
-    bag = DpnBag.where(object_identifier: self.identifier).first
-    bag
+	bag = DpnBag.where(object_identifier: self.identifier).first
+	bag
   end
 
   def gf_count
-    generic_files.where(state: 'A').count
+	generic_files.where(state: 'A').count
   end
 
   def gf_size
-    generic_files.where(state: 'A').sum(:size)
+	generic_files.where(state: 'A').sum(:size)
   end
 
   def active_files
-    generic_files.where(state: 'A')
+	generic_files.where(state: 'A')
   end
 
   def deleted_files
-    generic_files.where(state: 'D')
+	generic_files.where(state: 'D')
   end
 
   def processing_files
-    generic_files.where(state: 'I')
+	generic_files.where(state: 'I')
   end
 
   def object_report
-    data = {
-        active_files: self.active_files.count,
-        processing_files: self.processing_files.count,
-        deleted_files: self.deleted_files.count,
-        bytes_by_format: self.bytes_by_format
-    }
+	data = {
+		active_files: self.active_files.count,
+		processing_files: self.processing_files.count,
+		deleted_files: self.deleted_files.count,
+		bytes_by_format: self.bytes_by_format
+	}
   end
 
   def all_files_deleted?
-    return false if generic_files.count == 0
-    (generic_files.count == generic_files.where(state: 'D').count) ? true : false
+	return false if generic_files.count == 0
+	(generic_files.count == generic_files.where(state: 'D').count) ? true : false
   end
 
   def too_big?
-    total_size = self.generic_files.sum(:size)
-    (total_size > Pharos::Application::DPN_SIZE_LIMIT) ? true : false
+	total_size = self.generic_files.sum(:size)
+	(total_size > Pharos::Application::DPN_SIZE_LIMIT) ? true : false
   end
 
   def serializable_hash (options={})
-    data = super(options)
-    data.delete('ingest_state')
-    if options.has_key?(:include) && options[:include].include?(:ingest_state)
-      if self.ingest_state.nil?
-        data['ingest_state'] = 'null'
-      else
-        state = JSON.parse(self.ingest_state)
-        data.merge!(ingest_state: state)
-      end
-    end
-    data.merge(
-        in_dpn: in_dpn?,
-        file_count: gf_count,
-        file_size: gf_size,
-        institution: self.institution.identifier
-    )
+	data = super(options)
+	data.delete('ingest_state')
+	if options.has_key?(:include) && options[:include].include?(:ingest_state)
+	  if self.ingest_state.nil?
+		data['ingest_state'] = 'null'
+	  else
+		state = JSON.parse(self.ingest_state)
+		data.merge!(ingest_state: state)
+	  end
+	end
+	data.merge(
+		in_dpn: in_dpn?,
+		file_count: gf_count,
+		file_size: gf_size,
+		institution: self.institution.identifier
+	)
   end
 
   # Returns the WorkItem describing the last ingested
   # version of this object.
   def last_ingested_version
-    WorkItem.last_ingested_version(self.identifier)
+	WorkItem.last_ingested_version(self.identifier)
   end
 
   def add_event(attributes)
-    event = self.premis_events.build(attributes)
-    event.intellectual_object = self
-    event.institution = self.institution
-    event.save!
-    event
+	event = self.premis_events.build(attributes)
+	event.intellectual_object = self
+	event.institution = self.institution
+	event.save!
+	event
   end
 
   # Is the user allowed to discover this object?
   def can_discover?(user)
-    case access
-      when 'consortia'
-          true
-      when 'institution'
-          user.admin? || user.institution_id == institution_id
-      when 'restricted'
-          user.admin? || (user.institional_admin? && user.institution_id == institution_id)
-    end
+	case access
+	  when 'consortia'
+		  true
+	  when 'institution'
+		  user.admin? || user.institution_id == institution_id
+	  when 'restricted'
+		  user.admin? || (user.institional_admin? && user.institution_id == institution_id)
+	end
   end
 
   # Is the user allowed to read this object?
   def can_read?(user)
-    case access
-      when 'consortia'
-          true
-      when 'institution'
-          user.admin? || user.institution_id == institution_id
-      when 'restricted'
-          user.admin? || (user.institional_admin? && user.institution_id == institution_id)
-    end
+	case access
+	  when 'consortia'
+		  true
+	  when 'institution'
+		  user.admin? || user.institution_id == institution_id
+	  when 'restricted'
+		  user.admin? || (user.institional_admin? && user.institution_id == institution_id)
+	end
   end
 
   # Is the user allowed to edit this object?
   def can_edit?(user)
-    case access
-      when 'consortia'
-          user.admin? || user.institution_id == institution_id
-      when 'institution'
-          user.admin? || (user.institional_admin? && user.institution_id == institution_id)
-      when 'restricted'
-          user.admin? || (user.institional_admin? && user.institution_id == institution_id)
-    end
+	case access
+	  when 'consortia'
+		  user.admin? || user.institution_id == institution_id
+	  when 'institution'
+		  user.admin? || (user.institional_admin? && user.institution_id == institution_id)
+	  when 'restricted'
+		  user.admin? || (user.institional_admin? && user.institution_id == institution_id)
+	end
   end
 
 
   private
 
   def has_right_number_of_checksums(checksum_list)
-    checksums_okay = true
-    if(checksum_list.nil? || checksum_list.length == 0)
-      checksums_okay = false
-    else
-      algorithms = Array.new
-      checksum_list.each do |cs|
-        if (algorithms.include? cs)
-          checksums_okay = false
-        else
-          algorithms.push(cs)
-        end
-      end
-    end
-    checksums_okay
+	checksums_okay = true
+	if(checksum_list.nil? || checksum_list.length == 0)
+	  checksums_okay = false
+	else
+	  algorithms = Array.new
+	  checksum_list.each do |cs|
+		if (algorithms.include? cs)
+		  checksums_okay = false
+		else
+		  algorithms.push(cs)
+		end
+	  end
+	end
+	checksums_okay
   end
 
   def check_for_associations
-    # Check for related GenericFiles
-    unless generic_file_ids.empty?
-      errors[:base] << "Cannot delete #{self.id} because Generic Files are associated with it"
-    end
-    if errors[:base].empty?
-      true
-    else
-      throw(:abort)
-    end
+	# Check for related GenericFiles
+	unless generic_file_ids.empty?
+	  errors[:base] << "Cannot delete #{self.id} because Generic Files are associated with it"
+	end
+	if errors[:base].empty?
+	  true
+	else
+	  throw(:abort)
+	end
   end
 
   def set_bag_name
-    return if self.identifier.nil?
-    if self.bag_name.nil? || self.bag_name == ''
-      pieces = self.identifier.split('/')
-      i = 1
-      while i < pieces.count do
-        (i == 1) ? name = pieces[1] : name = "#{name}/#{pieces[i]}"
-        i = i+1
-      end
-      self.bag_name = name
-    end
+	return if self.identifier.nil?
+	if self.bag_name.nil? || self.bag_name == ''
+	  pieces = self.identifier.split('/')
+	  i = 1
+	  while i < pieces.count do
+		(i == 1) ? name = pieces[1] : name = "#{name}/#{pieces[i]}"
+		i = i+1
+	  end
+	  self.bag_name = name
+	end
   end
 
   def storage_option_is_allowed
-    unless Pharos::Application::PHAROS_STORAGE_OPTIONS.include?(self.storage_option)
-      errors.add(:storage_option, 'Storage Option is not one of the allowed options')
-    end
+	unless Pharos::Application::PHAROS_STORAGE_OPTIONS.include?(self.storage_option)
+	  errors.add(:storage_option, 'Storage Option is not one of the allowed options')
+	end
   end
 
   def freeze_storage_option
-    errors.add(:storage_option, 'cannot be changed') if self.saved_change_to_storage_option? unless self.storage_option.nil?
+	errors.add(:storage_option, 'cannot be changed') if self.storage_option_changed? unless self.storage_option.nil?
   end
 
 end
