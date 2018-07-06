a2a725a87f434e3e006df96b36bbd8f672f738ca
Kelly Croswell <kelly.croswell@aptrust.org>
2018-07-06 17:50:02 -0400

    updated migration and link and object view page

--- db/migrate/20180706172819_change_bagging_group_identifier_to_bag_group_identifier.rb
+++ db/migrate/20180706172819_change_bagging_group_identifier_to_bag_group_identifier.rb
@@ -2,20 +2,23 @@ class ChangeBaggingGroupIdentifierToBagGroupIdentifier < ActiveRecord::Migration
  def up
     puts "This migration takes a long time to run!"
     rename_column :intellectual_objects, :bagging_group_identifier, :bag_group_identifier
-    change_column :intellectual_objects, :bag_group_identifier, :string, null: false, default: ""
 
     batch_size = 5000
     count = 0
     IntellectualObject.find_in_batches(batch_size: batch_size) do |batch|
       IntellectualObject.transaction do
         batch.each do |io|
-          io.bag_group_identifier = ''
-          io.save!
+          if io.bag_group_identifier.nil?
+            io.bag_group_identifier = ''
+            io.save!
+          end
         end
       end
       count += 1
       puts "[#{Time.now}] Updated #{count * batch_size} IntellectualObjects"
     end
+
+    change_column :intellectual_objects, :bag_group_identifier, :string, null: false, default: ""
   end
 
   def down
