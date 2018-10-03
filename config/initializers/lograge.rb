81414c4dd3d738c8e7615ee07161f27d580d933c
Christian Dahlhausen <dahlhausen@gmail.com>
2018-10-03 10:18:21 -0400

    Disable logfile for testing

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
     config.lograge.enabled = true
-      config.lograge.formatter = Lograge::Formatters::KeyValue.new
-      config.lograge.keep_original_rails_log = true
-        config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+ #     config.lograge.formatter = Lograge::Formatters::KeyValue.new
+#      config.lograge.keep_original_rails_log = true
+#        config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
 end
