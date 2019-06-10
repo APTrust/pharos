4aa5482cf6943340c657ab17592a6a068c2136b2
Kelly Croswell <kelly.croswell@aptrust.org>
2019-06-10 09:10:28 -0400

    first step of migrations for bucket columns

--- db/migrate/20190531163431_add_buckets_to_institution.rb
+++ db/migrate/20190531163431_add_buckets_to_institution.rb
@@ -1,9 +1,9 @@
 class AddBucketsToInstitution < ActiveRecord::Migration[5.2]
   def up
-    add_column :institutions, :repo_receiving_bucket, :string, default: nil
-    add_column :institutions, :repo_restore_bucket, :string, default: nil
-    add_column :institutions, :demo_receiving_bucket, :string, default: nil
-    add_column :institutions, :demo_restore_bucket, :string, default: nil
+    add_column :institutions, :repo_receiving_bucket, :string, default: ""
+    add_column :institutions, :repo_restore_bucket, :string, default: ""
+    add_column :institutions, :demo_receiving_bucket, :string, default: ""
+    add_column :institutions, :demo_restore_bucket, :string, default: ""
   end
 
   def down
