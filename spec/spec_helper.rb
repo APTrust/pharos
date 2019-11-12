5456d14f5f06b237197ecb313c594de6d777829b
Kelly Croswell <kelly.croswell@aptrust.org>
2019-11-12 18:21:39 -0500

    added abort and message to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if (Rails.env.production? || Rails.env.demo?)
 require 'dotenv'
 Dotenv.load('.env.local', '.env.test','.env')
 require 'rspec/rails'
