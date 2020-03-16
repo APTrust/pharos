f951247aa35bf763d471c79f3638add899b94a33
A. Diamond <diamond@sheprador.com>
2020-03-16 09:08:42 -0400

    Checksum.datetime is now a datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_154633) do
+ActiveRecord::Schema.define(version: 2020_03_05_130017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_154633) do
 
   create_table "checksums", id: :serial, force: :cascade do |t|
     t.string "algorithm"
-    t.string "datetime"
+    t.datetime "datetime"
     t.string "digest"
     t.integer "generic_file_id"
     t.datetime "created_at", null: false
