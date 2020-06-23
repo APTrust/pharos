49bc992a8d4e82c2bcc88dd52f8f2555c622caa2
A. Diamond <diamond@sheprador.com>
2020-06-23 16:30:08 -0400

    Try to fix redis URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-Redis.current = Redis.new(url:  ENV['REDIS_URL'] || 'redis:localhost',
+Redis.current = Redis.new(url:  ENV['REDIS_URL'] || 'http://localhost',
                           port: ENV['REDIS_PORT'] || 6379,
                           db:   ENV['REDIS_DB'] || 0)
